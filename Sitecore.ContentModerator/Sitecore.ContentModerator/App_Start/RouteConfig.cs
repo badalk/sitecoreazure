9c4fc62d8a22938ce6e4ec46a5e624244b482493
pkarat <prasanth.karat@mindtree.com>
2017-03-04 12:18:01 -0500

    Disabled default routeconfig

--- Sitecore.ContentModerator/Sitecore.ContentModerator/App_Start/RouteConfig.cs
+++ Sitecore.ContentModerator/Sitecore.ContentModerator/App_Start/RouteConfig.cs
@@ -13,11 +13,11 @@ namespace Sitecore.ContentModerator
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
+            /*routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            );*/
         }
     }
 }
