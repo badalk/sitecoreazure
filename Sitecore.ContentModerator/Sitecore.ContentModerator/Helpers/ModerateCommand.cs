05a48b62d5dca523418ff7b887a7837b7c3f0d22
pkarat <prasanth.karat@mindtree.com>
2017-03-04 10:24:28 -0500

    Added Helper and config

---
+++ Sitecore.ContentModerator/Sitecore.ContentModerator/Helpers/ModerateCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.ContentModerator.Helpers
+{
+    public class ModerateCommand : Command, ISupportsContinuation
+    {
+        public override void Execute(CommandContext context)
+        {
+            try
+            {
+                Assert.ArgumentNotNull(context, "context");
+                if (context.Items.Length == 1)
+                {
+                    //Check if the tab is already open and refresh it is needed
+                    if (WebUtil.GetFormValue("scEditorTabs").Contains("ModerateContent:ModerateContentCommand"))
+                    {
+
+                    }
+                    else //Open a new tab
+                    {
+                        UrlString urlString = new UrlString("/Admin/ContentModerator.aspx");
+                        urlString.Append("itemId", context.Items[0].ID.ToString());
+                        context.Items[0].Uri.AddToUrlString(urlString);
+                        UIUtil.AddContentDatabaseParameter(urlString);
+                        SheerResponse.Eval(new ShowEditorTab
+                        {
+                            Command = "ModerateContent:ModerateContentCommand",
+                            Header = "Reviewing Content",
+                            Icon = Images.GetThemedImageSource("Applications/32x32/document_ok.png"),
+                            Url = urlString.ToString(),
+                            Id = "ModerateContent",
+                            Closeable = true
+                        }.ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Sitecore.Diagnostics.Log.Error("Error setting context menu for moderation", ex, "ContentModerator");
+            }
+        }
+    }
+}
